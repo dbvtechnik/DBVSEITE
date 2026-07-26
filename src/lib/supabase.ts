322a1634d8f9f00d208c8cf8f162398f31070163
dbvtechnik <info@dbv-veranstaltungstechnik.de>
2026-07-26 21:11:02 +0200

    Fix Admin Login Auth Header Issue

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -11,7 +11,6 @@ export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   global: {
     headers: {
       apikey: supabaseAnonKey,
-      Authorization: `Bearer ${supabaseAnonKey}`,
     },
   },
 });
